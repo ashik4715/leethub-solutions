d70a27d4a1ba5c77e57db7b4dfd5d028514587bb
ashik4715 <ashikurjhalak@gmail.com>
2025-09-03 11:14:43 +0600

    mergeSort from neetcode

---
+++ mergeSort.js
@@ -0,0 +1,32 @@
+// Merge Sort Implementation in JavaScript
+// Time Complexity: O(nlogn)
+// Space Complexity: O(n)
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+
+  const mid = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0, mid));
+  const right = mergeSort(arr.slice(mid));
+
+  return merge(left, right);
+}
+
+function merge(left, right) {
+  const result = [];
+  let i = 0,
+    j = 0;
+
+  while (i < left.length && j < right.length) {
+    if (left[i] <= right[j]) {
+      result.push(left[i]);
+      i++;
+    } else {
+      result.push(right[j]);
+      j++;
+    }
+  }
+  return result.concat(left.slice(i)).concat(right.slice(j));
+}
+
+const newArr = [3, 2, 4, 1, 6];
+console.log(mergeSort(newArr));
